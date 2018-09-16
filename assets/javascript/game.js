e0bb65bd6feab1aaa92da579bb531354fc1ccc0a
hoang <hoangcao@mail.weber.edu>
2018-09-16 17:35:08 -0600

    add fig

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,28 @@
+var animals = ["dog","cat","rabbit","hamster","bird","turtle","hermit crab",
+"chicken","teacup pig","salamander","pygmy goat","frog"]
 
-var url = "http://api.giphy.com/v1/gifs/search?q=" + "&api_key=yLhCT9SVV5K1G6OEs6Tw2AxbJVSKqWw2&limit=10";
-var pOne = $("<p>").text("Rating: ");
+renderButtons();
 
-$(".gif").append(pOne);
+$(".bt").on("click", function()
+{
+    console.log($(".bt").text());
+})
 
 $(".submit").on("click", function()
 {
-    
+    event.preventDefault();
+    var animal = $("#usr").val().trim();
+    if(animal != "")
+    {
+        animals.push(animal);
+        renderButtons();
+    }
 })
-function getAPI()
+
+$(document).on("click", ".animal-bt", function()
 {
+    var animal = $(this).attr("data-name");
+    var url = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=yLhCT9SVV5K1G6OEs6Tw2AxbJVSKqWw2&limit=10";
     fetch(url)  
     .then(function(r)
     {
@@ -17,12 +30,37 @@ function getAPI()
     })
     .then(function(data)
     {
-        console.log(data);
+        for(i = 0; i<10; i++)
+        {
+            console.log(data.data[i].images)
+            var rate = data.data[i].rating;
+            var imgURL = data.data[i].images.original.url; 
+            console.log(imgURL)           ;
+            var image = $("<img>").attr("src", imgURL);
+            var pOne = $("<p>").text("Rating: " + rate);
+            $(".gif").append(pOne);
+            $(".gif").append(image);
+        }
     })
-    .then(function(err)
+    .catch(function(err)
     {
         console.error(err);
     })
-}
+});
+
+
+function renderButtons() 
+{
+    $("#buttonView").empty();
+    for (var i = 0; i < animals.length; i++) {
+      var a = $("<button>");
+      a.addClass("btn btn-primary animal-bt");
+      a.attr("data-name", animals[i]);
+      a.text(animals[i]);
+      $("#buttonView").append(a);
+    }
+  }
+
+
 
 
